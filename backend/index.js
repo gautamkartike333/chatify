08e0a356d66fb3f0e41b7205288a9fe7cfd22ddd
gautamkartike333 <gautamkartike333@gmail.com>
2025-08-27 14:02:54 +0530

    last deploy changes

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ app.use('/api/user',userRouter)
 app.use(express.static(path.join(__dirname,"/frontend/dist")))
 
 app.get("*",(req,res)=>{
-    res.sendFile(path.join(__dirnamedirname,"frontend","dist","index.html"))
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
 })
 
 const PORT=process.env.PORT || 3000
--- backend/utils/jwtwebToken.js
+++ backend/utils/jwtwebToken.js
@@ -9,7 +9,7 @@ export const jwtToken = async (userId,res)=>{
     maxAge: 30*24*60*60*1000,
     httpOnly:true,
     sameSite:"strict",
-    secure: process.env.SECURE !== "development"
+    secure: true
 
 })
 }
