b7c177f23296d3b620f733f6540e389db82e1471
gautamkartike333 <gautamkartike333@gmail.com>
2025-08-27 14:30:07 +0530

    last deploy #

--- backend/utils/jwtwebToken.js
+++ backend/utils/jwtwebToken.js
@@ -1,15 +1,15 @@
 import jwt from "jsonwebtoken"
 
 
-export const jwtToken = async (userId,res)=>{
+export const jwtToken = async (userId, res) => {
 
-   const token = jwt.sign({userId} , process.env.JWT_SECRET);
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET);
 
-   res.cookie('jwt' , token , {
-    maxAge: 30*24*60*60*1000,
-    httpOnly:true,
-    sameSite:"strict",
-    secure: true
+    res.cookie('jwt', token, {
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+        sameSite: "strict",
+        secure: true
 
-})
+    })
 }
